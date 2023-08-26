08a33b25534d0cabe46cbf521528b83d781ee45a
Vignesh-S511 <vigneshsankar9750@gmail.com>
2023-08-26 20:41:45 +0530

    get the input from the user

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Date;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number;
+        while(true) {
+            number = scanner.nextInt();
+            scanner.nextLine();
+            switch (number) {
+                case 1:
+                    String userName = scanner.nextLine();
+                    String password = scanner.nextLine();
+                    String dob = scanner.nextLine();
+                    String gender = scanner.nextLine();
+                    String qualification = scanner.nextLine();
+                    System.out.println("vignesh");
+            }
+        }
 
     }
 }
+   /* Placement Portal:
+        1. New user registration
+        2. user login
+        3. New company registration
+        4. company login
+        5. company -> vacancy-> y/n ; required skills
+
+        6. user applies to the company
+        7. accept/reject user
+        8. stop*/
